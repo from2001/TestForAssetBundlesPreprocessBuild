7199375d7cdd5851900c6eaf1d47aa6bc66765ee
Masahiro Yamaguchi <from2001@hotmail.com>
2024-05-30 10:53:09 +0900

    Updated Samples

--- Assets/_ARTrackedImagePreprocessBuild/PreprocessBuildTest.cs
+++ Assets/_ARTrackedImagePreprocessBuild/PreprocessBuildTest.cs
@@ -3,16 +3,12 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class BuildAssetBundle : MonoBehaviour
+public class PreprocessBuildTest : MonoBehaviour
 {
-
     [MenuItem("Test/AR Tracked Image PreprocessBuild")]
     static void ExportAssetBundles()
     {
         UnityEditor.XR.ARSubsystems.ARBuildProcessor.PreprocessBuild(BuildTarget.VisionOS);
         Debug.Log("PreprocessBuild completed");
     }
-
-
-
 }
